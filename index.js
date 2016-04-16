8513e404be1abd054b5ed81ce6e1b10a448bd34f
Nadimozzaman Pappo <mnpappo@gmail.com>
2016-04-16 18:21:53 +0600

    server url changed

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://atsuser:atsuser123@ats-api-2236.mongo.dbs.appsdeck.eu/ats-api-2236',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   javascriptKey: process.env.JAVASCRIPT_KEY || 'jsKey',
   masterKey: process.env.MASTER_KEY || 'masterKey', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://ats-api.scalingo.io/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
